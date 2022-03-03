d96ac2c5f81f5c6012e831b794e5e819ba8782fd
TsuyoshiMiyazaki <88712005+TsuyoshiMiyazaki@users.noreply.github.com>
2022-03-03 21:46:57 +0900

    Update distance.py

--- Miyazaki/distance.py
+++ Miyazaki/distance.py
@@ -1,17 +1,18 @@
-//距離を出す
+#距離を出す
 
 import math
 
 pi=math.pi
 
-Goal_lat=
-Goal_lng=
+Goal_lat=a #ゴール地点の経度（決まったら入れる）
+Goal_lng=b #ゴール地点の緯度（決まったら入れる）
 
 gps_lat=data_stream.TPV['lat']
 gps_lng=data_stream.TPV['lon']
 
 delta_lng=Goal_lng-gps_lng
 omega=math.acos(math.sin(gps_lat*2*pi/360)*math.sin(Goal_lat*2*pi/360)+math.cos(gps_lat*2*pi/360)*math.cos(Goal_lat*2*pi/360)*math.cos(delta_lng*2*pi/360))
-distance=6378.137*10**3*omega
 
-
+def CalculateDis():
+    return 6378.137*10**3*omega
+    
