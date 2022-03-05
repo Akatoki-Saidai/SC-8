bcd26fadda5f43f931af0688959e5e477bdc17f4
TsuyoshiMiyazaki <88712005+TsuyoshiMiyazaki@users.noreply.github.com>
2022-03-06 02:03:09 +0900

    Create far_case.py

---
+++ Miyazaki/far_case.py
@@ -0,0 +1,13 @@
+#距離が遠いとき
+
+while now_dis>3:
+  now_dis=CalculateDis()
+  now_ang=CalculateAngle()
+  forward()
+  next_dis=CalculateDis()
+  next_ang=CalculateAngle()
+  delta_rad=next_ang-now_ang
+  if delta_rad>0:
+    turn_left()
+  else:
+    turn_right()
