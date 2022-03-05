22269914f18563fd13186de9bf309b7f19f8573f
TsuyoshiMiyazaki <88712005+TsuyoshiMiyazaki@users.noreply.github.com>
2022-03-06 02:04:36 +0900

    Update far_case.py

--- Miyazaki/far_case.py
+++ Miyazaki/far_case.py
@@ -4,7 +4,6 @@ while now_dis>3:
   now_dis=CalculateDis()
   now_ang=CalculateAngle()
   forward()
-  next_dis=CalculateDis()
   next_ang=CalculateAngle()
   delta_rad=next_ang-now_ang
   if delta_rad>0:
